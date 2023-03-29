9e975765a7083d4efe4ae493cbd3caa1d75570f0
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-03-29 21:45:30 +0000

    ProductCollection Created

---
+++ app/Http/Resources/Product/ProductCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'totalPrice' => round((1 - ($this->discount / 100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 0) : 'No rating yet',
+            'href' => [
+                'link' => route('products.show', $this->id)
+            ]
+        ];
+    }
+}
