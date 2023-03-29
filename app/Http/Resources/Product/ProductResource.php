4aa000c354d2a622512c04f3d803581908228e9e
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-03-29 21:45:50 +0000

    ProductResource Created

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'code_bare' => $this->code_bare,
+            'discount' => $this->discount,
+            'totalPrice' => round((1 -($this->discount/100)) * $this->price, 2),
+            'old_price' => $this->old_price,
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 0) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
+        ];
+    }
+}
