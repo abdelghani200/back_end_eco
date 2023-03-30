06467557f52e92f5c8c4865b9d5bc15391c8bfac
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-03-30 16:23:57 +0000

    CategorieFactory created

---
+++ database/factories/CategorieFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Categorie>
+ */
+class CategorieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $faker = \Faker\Factory::create();
+
+        return [
+            'name' => $faker->word,
+            'description' => $faker->paragraph,
+            'image' =>  $faker->image('public/storage/images/categories',640,480, null, false),
+        ];
+    }
+}
