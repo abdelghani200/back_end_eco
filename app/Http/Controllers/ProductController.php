0c1a7d62f519492e36e946d7cc94ce4c7e27ae8b
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-03-30 11:26:08 +0000

    store & update & destroy product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
 
 class ProductController extends Controller
 {
+
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:api')->except('index', 'show');
+    // }
+
     /**
      * Display a listing of the resource.
      */
@@ -28,9 +37,20 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->old_price = $request->old_price;
+        $product->code_bare = $request->code_bare;
+        $product->discount = $request->discount;
+        $product->save();
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
@@ -52,16 +72,19 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->all());
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response(null,Response::HTTP_NO_CONTENT);
+        
     }
 }
