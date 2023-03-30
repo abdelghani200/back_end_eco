7192e1f5bd0263a8cb8b1605e1e2955ab024beaf
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-03-30 13:41:22 +0000

    created an show review

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Review;
 use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Requests\ReviewRequest;
 use App\Http\Resources\ReviewResource;
 use App\Http\Requests\StoreReviewRequest;
 use App\Http\Requests\UpdateReviewRequest;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class ReviewController extends Controller
 {
@@ -29,9 +33,17 @@ class ReviewController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreReviewRequest $request)
+    public function store(ReviewRequest $request, Product $product)
     {
-        //
+
+        $review = new Review($request->all());
+
+        $product->reviews()->save($review);
+
+        return response([
+            'data' => new ReviewResource($review)
+        ], Response::HTTP_CREATED);
+
     }
 
     /**
@@ -39,7 +51,7 @@ class ReviewController extends Controller
      */
     public function show(Review $review)
     {
-        //
+        return new ReviewResource($review);
     }
 
     /**
