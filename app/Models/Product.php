e2c232cad6a0e60220cd930a76aa9351115f1282
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-03-30 16:25:43 +0000

    create relation_shep

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Review;
+use App\Models\Categorie;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -18,11 +19,17 @@ class Product extends Model
         'code_bare',
         'old_price',
         'discount',
+        'category_id'
     ];
 
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Categorie::class);
     }
 
 }
