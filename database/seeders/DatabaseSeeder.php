5915e75e7a53c815c33638e27e276bc94f434ccf
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-03-30 16:23:19 +0000

    UserSeeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Categorie;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
 use Database\Seeders\ReviewSeeder;
 use Database\Seeders\ProductSeeder;
 
@@ -18,12 +21,14 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProductSeeder::class,
             ReviewSeeder::class,
+            CategorieSeeder::class,
+            UserSeeder::class
         ]);
 
-        \App\Models\User::factory()->create([
-            'name' => 'Admin User',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt(123456),
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Admin User',
+        //     'email' => 'admin@gmail.com',
+        //     'password' => bcrypt(123456),
+        // ]);
     }
 }
