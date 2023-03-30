65061b375df3632ce0098759d14fa3309bc7901b
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-03-30 16:22:14 +0000

    ControllerCategories Created

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categorie;
+use App\Http\Requests\StoreCategorieRequest;
+use App\Http\Requests\UpdateCategorieRequest;
+use App\Http\Resources\Categorie\CategorieResource;
+use App\Http\Resources\Categorie\CategorieCollection;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CategorieCollection::collection(Categorie::paginate(20));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategorieRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Categorie $categorie)
+    {
+        return new CategorieResource($categorie);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategorieRequest $request, Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Categorie $categorie)
+    {
+        //
+    }
+}
