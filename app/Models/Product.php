499adbb53fcb110cba07093a8eead3e02aa01d92
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-04-20 15:33:43 +0000

    update Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\Cart;
+use App\Models\User;
+use App\Models\Order;
 use App\Models\Review;
 use App\Models\Categorie;
 use Illuminate\Database\Eloquent\Model;
@@ -13,13 +16,14 @@ class Product extends Model
 
     protected $fillable = [
         'name',
+        'image',
         'description',
         'price',
         'stock',
         'code_bare',
         'old_price',
         'discount',
-        'category_id'
+        'categorie_id'
     ];
 
     public function reviews()
@@ -32,4 +36,26 @@ class Product extends Model
         return $this->belongsTo(Categorie::class);
     }
 
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'carts', 'product_id', 'user_id')->withPivot('quantity');
+    }
+
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function order()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class, 'order_produit', 'produit_id', 'order_id')->withPivot('quantite');
+    }
+
+
+   
 }
