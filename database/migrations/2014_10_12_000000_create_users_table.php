817fa601cf0189f89c0f1a537782c046a9eefdd1
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-04-20 15:36:24 +0000

    update migration users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastName');
+            $table->string('address');
+            $table->string('ville');
+            $table->string('password_confirmation')->nullable();
+            $table->integer('numTele');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
