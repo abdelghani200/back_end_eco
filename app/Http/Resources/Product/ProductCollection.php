29b5150fd69237581d7315692d5f55edc72ac2ff
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-04-20 15:36:04 +0000

    product collection Created

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductCollection extends JsonResource
@@ -14,9 +15,22 @@ class ProductCollection extends JsonResource
      */
     public function toArray(Request $request): array
     {
+
+        $category = DB::table('categories')->where('id', $this->categorie_id)->first();
+
         return 
         [
+            'id' => $this->id,
+            // 'categorie_id' =>$this->categorie_id,
             'name' => $this->name,
+            'category' => optional($category)->name,
+            // 'category_id' => optional($category)->id,
+            'image' => $this->image,
+            'description' => $this->description,
+            'price' => $this->price,
+            'stock' => $this->stock,
+            'code_bare' => $this->code_bare,
+            'old_price' => $this->old_price,
             'totalPrice' => round((1 -($this->discount/100)) * $this->price, 2),
             'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 0) : 'No rating yet',
             'href' =>[
